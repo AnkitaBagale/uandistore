18be07eaf213737edb8f33827be5e9abde9e7305
ankitabagale <manali.bagale@gmail.com>
2021-04-30 01:50:20 +0530

    add: playlist model description

--- models/playlist.model.js
+++ models/playlist.model.js
@@ -5,6 +5,7 @@ const { Schema } = mongoose;
 const PlaylistSchema = new Schema({
     userId: {type: Schema.Types.ObjectId, ref: "User"},
     title: {type: String},
+    description: {type: String, default: ""},
     thumbnail:{type: String, default: "https://i.postimg.cc/TwsBcV04/jess-bailey-l3-N9-Q27z-ULw-unsplash.jpg"},
     type: {type: String, enum:["custom", "watchlater", "liked", "history"], default: "custom"},
     isDefault: {type: Boolean, default: false},
