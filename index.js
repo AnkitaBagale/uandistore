505f546f6209ce9992c4b3e5a6095b43e5169e57
ankitabagale <manali.bagale@gmail.com>
2021-05-04 17:18:40 +0530

    fix: video route error

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const products = require("./routes/products.router");
-const carts = require("./routes/carts.router");
-const users = require("./routes/users.router");
-const wishlists = require("./routes/wishlists.router");
-const addresses = require("./routes/addresses.router");
+const products = require('./routes/products.router');
+const carts = require('./routes/carts.router');
+const users = require('./routes/users.router');
+const wishlists = require('./routes/wishlists.router');
+const addresses = require('./routes/addresses.router');
 
-const tutors = require("./routes/tutors.router");
-const videos = require("./routes/videos.router");
-const notes = require("./routes/notes.router");
-const playlists = require("./routes/playlists.router");
+const tutors = require('./routes/tutors.router');
+const videos = require('./routes/videos.router');
+const notes = require('./routes/notes.router');
+const playlists = require('./routes/playlists.router');
 
-const routeNotFoundHandler = require("./middlewares/route-not-found.middlerware");
-const allErrorsHandler = require("./middlewares/all-errors-handler.middleware");
-const initializeConnectionToDb = require("./db/db.connect");
-
+const routeNotFoundHandler = require('./middlewares/route-not-found.middlerware');
+const allErrorsHandler = require('./middlewares/all-errors-handler.middleware');
+const initializeConnectionToDb = require('./db/db.connect');
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(cors());
 
 const port = 3000;
 
 initializeConnectionToDb();
 
-app.get("/", (req,res)=>{
-  res.send("Welcome to the U&I store");
-})
+app.get('/', (req, res) => {
+	res.send('Welcome to the U&I store');
+});
 
-app.use("/products", products);
-app.use("/wishlists", wishlists);
-app.use("/carts", carts);
-app.use("/users", users);
-app.use("/users", addresses);
+app.use('/products', products);
+app.use('/wishlists', wishlists);
+app.use('/carts', carts);
+app.use('/users', users);
+app.use('/users', addresses);
 
-app.use("/tutors", tutors);
-app.use("/videos", videos);
-app.use("/notes", notes);
-app.use("/playlists", playlists);
+app.use('/tutors', tutors);
+app.use('/videos', videos);
+app.use('/notes', notes);
+app.use('/playlists', playlists);
 
 /**
  * 404 Route Handler
@@ -54,7 +53,6 @@ app.use(routeNotFoundHandler);
  */
 app.use(allErrorsHandler);
 
-
-app.listen( process.env.PORT || port, () => {
-    console.log(`server started`)
-  })
+app.listen(process.env.PORT || port, () => {
+	console.log(`server started`);
+});
--- routes/videos.router.js
+++ routes/videos.router.js
@@ -1,52 +1,62 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Video = require("../models/video.model");
-const {extend} = require("lodash");
+const Video = require('../models/video.model');
+const { extend } = require('lodash');
 
+router
+	.route('/')
+	.get(async (req, res) => {
+		try {
+			const videos = await Video.find({}).populate('tutorId');
+			res.status(200).json({ response: videos, success: true });
+		} catch (error) {
+			res.status(500).json({
+				success: false,
+				message: 'Something went wrong',
+				errorMessage: error.message,
+			});
+		}
+	})
+	.post(async (req, res) => {
+		try {
+			const videoDetails = req.body;
+			let videoIfExists = await Video.findById(videoDetails._id);
+			if (videoIfExists) {
+				videoIfExists = extend(videoIfExists, videoDetails);
+				videoIfExists = await videoIfExists.save();
+				res.status(200).json({ response: videoIfExists, success: true });
+			} else {
+				let NewVideo = new Video(videoDetails);
+				NewVideo = await NewVideo.save();
+				res.status(201).json({ response: NewVideo, success: true });
+			}
+		} catch (error) {
+			res.status(500).json({
+				success: false,
+				message: 'Something went wrong',
+				errorMessage: error.message,
+			});
+		}
+	});
 
-router.route("/")
-.get(async(req,res)=>{
-    try {
-        const videos = await Video.find({}).populate("tutorId");
-
-        res.status(200).json({response: videos, success: true});
-    } catch(error) {
-        res.status(500).json({ success: false, message: "Something went wrong", errorMessage: error.message})
-    }
-    
-})
-.post(async(req,res)=>{
-    try {
-        const videoDetails = req.body;
-        let videoIfExists = await Video.findById(videoDetails._id);
-        if(videoIfExists) {
-            videoIfExists = extend(videoIfExists, videoDetails);
-            videoIfExists = await videoIfExists.save();
-            res.status(200).json({response: videoIfExists, success: true});
-        } else {
-            let NewVideo = new Video(videoDetails);
-            NewVideo = await NewVideo.save();
-            res.status(201).json({response: NewVideo, success: true});
-        }
-
-    } catch(error) {
-        res.status(500).json({ success: false, message: "Something went wrong", errorMessage: error.message})
-    }
-    
-})
-
-router.route("/:videoId")
-.get(async(req,res)=>{
-    try {
-        const {videoId} = req.params;
-        const video = await Video.findById(videoId).populate("tutorId");
-
-        res.status(200).json({response: video, success: true});
-        
-    } catch(error) {
-        res.status(500).json({ success: false, message: "Something went wrong", errorMessage: error.message})
-    }
-    
-})
+router.route('/:videoId').get(async (req, res) => {
+	try {
+		const { videoId } = req.params;
+		const video = await Video.findById(videoId).populate('tutorId');
+		if (video) {
+			res.status(200).json({ response: video, success: true });
+			return;
+		}
+		res
+			.status(404)
+			.json({ success: false, message: 'video associated with id not found' });
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: 'Something went wrong',
+			errorMessage: error.message,
+		});
+	}
+});
 
 module.exports = router;
